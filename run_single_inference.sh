70f1fb163fbbd2002491214042fb18f025f7732c
Chih-Cheng (CC) Chang <ccbs1012@gmail.com>
2025-02-18 00:43:10 +0800

    Update run_single_inference.sh

--- run_single_inference.sh
+++ run_single_inference.sh
@@ -1,1 +1,1 @@
-python3 single_inference.py --speaker_prompt_audio_path "data/example.wav" --speaker_prompt_text_transcription "在一部分是今年我會給大家一個，因為今年的狀況所以，我希望今年大家要做到一點是，應該說我接下來的目標會是，我會依據各個部門或個人，我可能會，更著重在" --content_to_synthesize "早上好 中國 現在我有冰七令" --output_path results/out.wav
+python3 single_inference.py --speaker_prompt_audio_path "data/example.wav"  --content_to_synthesize "早上好 中國 現在我有冰七令" --output_path results/out.wav
