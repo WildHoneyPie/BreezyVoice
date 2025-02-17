33d13dabe59e1c83b7e359456a7374e93a027f9f
Chih-Cheng (CC) Chang <ccbs1012@gmail.com>
2025-02-17 22:58:21 +0800

    Update run_single_inference.sh

--- run_single_inference.sh
+++ run_single_inference.sh
@@ -1,1 +1,2 @@
-python3 single_inference.py --speaker_prompt_audio_path "data/example.wav" --speaker_prompt_text_transcription "在你服務的這些年裡面，你有沒有，什麼樣的心境的變化，或者是說尤其是從，本來是服務，大概就是，緊急安置到下一個階段，但我相信立新這邊服務的恐怕是比較長期一點的，的內容。" --content_to_synthesize "早上好 中國 現在我有冰七令" --output_path results/out.wav
+python3 single_inference.py --speaker_prompt_audio_path "data/example.wav" --speaker_prompt_text_transcription "在一部分是今年我會給大家一個，因為今年的狀況所以，我希望今年大家要做到一點是，應該說我接下來的目標會是，我會依據各個部門或個人，我可能會，更著重在，大家能優化的地方去執行，然後我今天的眾，我今年的重點應該會放在團隊合作上，對就像，可能很多人都不理解我為什麼去年會逼著你們要做
+" --content_to_synthesize "早上好 中國 現在我有冰七令" --output_path results/out.wav
